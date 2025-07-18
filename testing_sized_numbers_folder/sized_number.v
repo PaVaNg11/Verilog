718ea79dae989944f847e9a341c6ef8f106f375f
pavang <pavanbit11@gmail.com>
2025-07-18 12:42:14 +0530

    latest commit

--- testing_sized_numbers_folder/sized_number.v
+++ testing_sized_numbers_folder/sized_number.v
@@ -3,9 +3,11 @@ module sized_number;
 reg [63:0] a,b,c;
 reg [8*20:0] str;
 
+reg [7:0] mem [0:1023];         // [size] mem [Depth]
+integer i,j;
 
 initial begin
-    a[31:0]='Hxab   c1;
+    a[31:0]='Hxabc1;
     b[31:0]='hzef;
     c=16'hz45;
     $display("a=%h | b=%h | c=%h",a,b,c);
@@ -13,7 +15,20 @@ initial begin
     str="HELLO123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $display("a=%h | b=%h | c=%h",a,b,c);
     $display("%s",str);
+    
+    for(i=0;i<1024;i=i+1) begin
+        mem[i]=~1'b0;
+        mem[i][0]=1'b0;
+        $display("VALUE:%b",mem[i]);
+    end
+
+
+
+
 end
+
+
+
 
 
 endmodule
